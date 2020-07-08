ee2cae616d9f6033078c670317b48361e9f59e98
Vishnu Ram V <vishnu.v1902@gmail.com>
2020-07-08 19:01:31 +0530

    Landing page added

--- Js/app.js
+++ Js/app.js
@@ -5,8 +5,7 @@ const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
-  lines[0].classList.toggle("open");
-  lines[2].classList.toggle("open");
+  lines[1].classList.toggle("open");
   links.forEach(link => {
     link.classList.toggle("fade");
   });
