cc01363f05fcdce879c8d403146d43c9c67cb128
Vishnu Ram V <vishnu.v1902@gmail.com>
2020-07-09 18:43:40 +0530

    Added preloader and made some minor changes

--- Js/app.js
+++ Js/app.js
@@ -1,17 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const lines = document.querySelectorAll(".line");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const navLinks = document.querySelector(".nav-linkss");
+const links = document.querySelectorAll(".nav-linkss li");
+const body = document.querySelector("body");
+
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   lines[1].classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
+  body.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fadee");
   });
 });
 
-navLinks.addEventListener("click", ()=>{
-  links.classList.remove  ("active");
-  this.classList.add("active");
-});
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 600) nav.className = ''; else nav.className = 'scroll';
+};
